4bf188451312e85a8cb1b3bcb0517dd08fcf62e6
Dmitry Sysoev <jdmitrys@yandex.ru>
2023-08-07 13:49:05 +0400

    REST architecture

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 @app.get("/")
-def read_root():
-    html_content = "<h1>FastAPI</h1>"
-    return HTMLResponse(content=html_content)
+def root():
+    return {"message": "FastAPI"}
+
+@app.get("/about")
+def about():
+    return {"message": "About"}
