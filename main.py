1c980ca63bc8ac621b8aa2eea267adbb3d8d1e5d
Dmitry Sysoev <jdmitrys@yandex.ru>
2023-08-26 19:05:54 +0400

    Different format response

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse, JSONResponse, HTMLResponse
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"message": "FastAPI"}
+@app.get("/text", response_class=PlainTextResponse)
+def root_text():
+    return "FastAPI"
 
-@app.get("/about")
-def about():
-    return {"message": "About"}
+@app.get("/html", response_class=HTMLResponse)
+def root_html():
+    return "<h1>Book</h1>"
