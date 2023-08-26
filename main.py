09e2d1c5911c0644990764bc3db0fcdf6c9f6d0d
Dmitry Sysoev <jdmitrys@yandex.ru>
2023-08-26 19:23:17 +0400

    Sending files from server

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from fastapi import FastAPI
-from fastapi.responses import PlainTextResponse, JSONResponse, HTMLResponse
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
-@app.get("/text", response_class=PlainTextResponse)
-def root_text():
-    return "FastAPI"
-
-@app.get("/html", response_class=HTMLResponse)
-def root_html():
-    return "<h1>Book</h1>"
+@app.get("/")
+def root():
+    return FileResponse("public/index.html",
+                        filename="mainpage.html",
+                        media_type="application/octet-streame")
