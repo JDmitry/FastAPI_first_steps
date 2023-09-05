47c5c182736926e0881158c9b4688eca247e21e4
Dmitry Sysoev <jdmitrys@yandex.ru>
2023-09-05 13:57:17 +0400

    Redirect

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from fastapi import FastAPI, Response, Path
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse, PlainTextResponse
  
 app = FastAPI()
- 
-@app.get("/users/{id}", status_code=200)
-def users(response: Response, id: int = Path()):
-    if id < 1:
-        response.status_code = 400
-        return {"message": "Incorrect Data"}
-    return  {"message": f"Id = {id}"}
+
+@app.get("/old")
+def old():
+    return RedirectResponse("https://metanit.com/python/fastapi")
+
+@app.get("/new")
+def new():
+    return PlainTextResponse("Новая страница")
