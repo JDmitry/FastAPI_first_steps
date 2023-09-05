6e3045fa4f4ab724b283e1c6eb7414b5136d888b
Dmitry Sysoev <jdmitrys@yandex.ru>
2023-09-05 14:35:05 +0400

    Static files

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 from fastapi import FastAPI
-from fastapi.responses import RedirectResponse, PlainTextResponse
+from fastapi.staticfiles import StaticFiles
  
 app = FastAPI()
 
-@app.get("/old")
-def old():
-    return RedirectResponse("https://metanit.com/python/fastapi")
-
-@app.get("/new")
-def new():
-    return PlainTextResponse("Новая страница")
+app.mount("/", StaticFiles(directory="public", html=True))
